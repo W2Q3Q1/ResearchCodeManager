448aaf70c8bcedc17f652a337ca8404682cdc184
宋子琪 <2716406885@qq.com>
2022-11-23 15:11:15 +0800

    调整一个（看文档）可能在其他系统上出现问题的写法

--- manager/main.cpp
+++ manager/main.cpp
@@ -16,25 +16,32 @@ int main(int argc, char *argv[])
     //单实例运行
     QSharedMemory shared_memory("manager");
     if(shared_memory.attach()){
+        shared_memory.detach();
         QMessageBox::critical(nullptr, QObject::tr("Critical Error"), QObject::tr("An instance has already been activated!"));
         return 0;
     }
     shared_memory.create(1);
-    //迫使代码创建一个CodeManager对象
-    CodeManager::getSelf();
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "research_code_manager_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
+    int result = 0;
+    try{
+        //迫使代码创建一个CodeManager对象
+        CodeManager::getSelf();
+        QTranslator translator;
+        const QStringList uiLanguages = QLocale::system().uiLanguages();
+        for (const QString &locale : uiLanguages) {
+            const QString baseName = "research_code_manager_" + QLocale(locale).name();
+            if (translator.load(":/i18n/" + baseName)) {
+                a.installTranslator(&translator);
+                break;
+            }
         }
-    }
 
-    ResearchCodeManager w;
-    w.show();
-    int result = a.exec();
-    CodeManager::closeSelf();
+        ResearchCodeManager w;
+        w.show();
+        int result = a.exec();
+        CodeManager::closeSelf();
+    }catch(...){
+
+    }
+    shared_memory.detach();
     return result;
 }
